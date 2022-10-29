6e5f26a9dc28c57c798bf170eefc166591772f04
Milan Jovanović <milannis.995@gmail.com>
2022-10-29 19:18:54 +0200

    Initial commit

---
+++ CurrentTimeService/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+
+await app.RunAsync();
